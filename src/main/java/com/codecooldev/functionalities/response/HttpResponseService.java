1a9fc1c21562452a4481d929fda3d8cfc693aac1
Kamil <blueflamesxcode@gmail.com>
2018-03-13 22:35:16 +0100

    adapt service for usage of response generator

--- src/main/java/com/codecooldev/functionalities/response/HttpResponseService.java
+++ src/main/java/com/codecooldev/functionalities/response/HttpResponseService.java
@@ -1,14 +1,11 @@
 package com.codecooldev.functionalities.response;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 
 public class HttpResponseService {
     private OutputStream os;
     private HttpResponse response;
-    private static final String CRLF = "\r\n";
 
     public HttpResponseService(OutputStream os, HttpResponse response) {
         this.os = os;
@@ -16,15 +13,19 @@ public class HttpResponseService {
     }
 
     public void sendResponse() {
-
         try (PrintWriter out = new PrintWriter(os)) {
-            out.print(String.format("HTTP/1.0 %d %s%s",response.getStatusCode(), response.getStatusInfo(), CRLF));
-            out.print(String.format("Date: %s%s", response.getDate(), CRLF));
-            out.print(String.format("Server: %s%s",response.getServer(), CRLF));
-            out.print(String.format("Content-Type: %s%s", response.getContentType(), CRLF));
-            out.print(String.format("Content-Length: %d%s", response.getContentLen(), CRLF));
-            out.println(String.format("Content-Type: %s; %s%s",response.getContentType(),response.getCoding(),CRLF));
-            out.println(response.getBody());
+            StringBuilder responseBuilder = ResponseGenerator.create()
+                     .addHeader("HTTP/1.0 %s %s", response.getStatusCode(), response.getStatusInfo())
+                         .addProperty("Date", "%s", response.getDate())
+                         .addProperty("Server", "%s", response.getServer())
+                         .addProperty("Content-Type", "%s", response.getContentType())
+                         .addProperty("Content-Length", "%s", response.getContentLen())
+                         .addProperty("Content-Type", "%s; %s", response.getContentType(), response.getCoding())
+                         .finishAddingProperties()
+                             .addBody(response.getBody())
+                                .getResponse();
+
+            out.print(responseBuilder.toString());
         }
     }
 }
