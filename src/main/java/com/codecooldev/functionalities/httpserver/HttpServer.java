03073b102325023e3dbd20bf7d4ee7ff9d607cc0
KonradLekawski <Snajperv52@gmail.com>
2018-03-14 14:08:58 +0100

    implement createContext method

--- src/main/java/com/codecooldev/functionalities/httpserver/HttpServer.java
+++ src/main/java/com/codecooldev/functionalities/httpserver/HttpServer.java
@@ -2,23 +2,21 @@ package com.codecooldev.functionalities.httpserver;
 
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpServer {
-    private InetAddress inetAddress;
-    private Integer port;
+    private int port;
+    private InetAddress ipAddress;
+    private Map<String,HttpHandler> handlers;
 
-    private HttpServer() {}
-
-    public static HttpServer create(String ipAddress, Integer port)
-            throws IOException {
-
-        HttpServer httpServer = new HttpServer();
-        httpServer.setIpAddress(InetAddress.getByName(ipAddress));
-        httpServer.setPort(port);
-        return httpServer;
+    private HttpServer() {
+        this.handlers = new HashMap<>();
     }
-
-    // TODO : add port validation (is empty)
 
     public static HttpServer create(Integer port) throws IOException {
         HttpServer httpServer = new HttpServer();
@@ -27,11 +25,42 @@ public class HttpServer {
         return httpServer;
     }
 
+    public void start() {
+
+        try (ServerSocket server = new ServerSocket(this.port)) {
+            ExecutorService executorService = Executors.newCachedThreadPool();
+            while (true) {
+                try {
+                    Socket connection = server.accept();
+                    EchoReceiver task = new EchoReceiver(connection, handlers);
+                    executorService.execute(task);
+                } catch (IOException ex) {}
+            }
+        } catch (IOException ex) {
+            System.err.println("Couldn't start server");
+        }
+    }
+
+    public void createContext(String path, HttpHandler handler) {
+        this.handlers.put(path,handler);
+    }
+
     private void setIpAddress(InetAddress ipAddress) {
-        this.inetAddress = ipAddress;
+        this.ipAddress = ipAddress;
     }
 
     private void setPort(Integer port) {
+        if (port < 0 || port > 0xFFFF)
+            throw new IllegalArgumentException(
+                    "Port value out of range: " + port);
         this.port = port;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public InetAddress getIpAddress() {
+        return ipAddress;
     }
 }
