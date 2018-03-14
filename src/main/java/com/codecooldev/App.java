f82e8b96a13173e55337688b7c1c7fb6251a3ee3
Kamil <blueflamesxcode@gmail.com>
2018-03-14 23:23:59 +0100

    put logic from Test class to real App starter

--- src/main/java/com/codecooldev/App.java
+++ src/main/java/com/codecooldev/App.java
@@ -1,33 +1,25 @@
 package com.codecooldev;
 
-import com.codecooldev.baseclasses.ApplicationException;
-import com.codecooldev.controller.Ctrl;
-import com.codecooldev.view.Screen;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import com.codecooldev.functionalities.httpserver.HttpServer;
+import com.codecooldev.functionalities.httpserver.MyHandler;
+
+import java.io.IOException;
 
 /**
  * Hello world!
  *
  */
-@Configuration
-@ComponentScan("com")
+
 public class App {
     public static void main(String[] args) {
-        ApplicationContext ctx = new AnnotationConfigApplicationContext(App.class);
-        Ctrl ctrl = ctx.getBean(Ctrl.class);
-        appRunner(ctrl, args);
-    }
-
-    private static void appRunner(Ctrl ctrl, String[] args) {
-        Screen.clear();
-
         try {
-            ctrl.start(args);
-        } catch (ApplicationException e) {
+            HttpServer httpServer = HttpServer.create(9999);
+            httpServer.setRoute("/", new MyHandler());
+            httpServer.start();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+
 }
