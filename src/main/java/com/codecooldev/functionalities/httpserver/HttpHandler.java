3aa9ceddba23b1bdf4ab1a1edb10a2451b6dbc59
KonradLekawski <Snajperv52@gmail.com>
2018-03-14 14:08:24 +0100

    implement HttpHandler interface

--- src/main/java/com/codecooldev/functionalities/httpserver/HttpHandler.java
+++ src/main/java/com/codecooldev/functionalities/httpserver/HttpHandler.java
@@ -2,6 +2,8 @@ package com.codecooldev.functionalities.httpserver;
 
 import com.codecooldev.functionalities.httprequest.HttpRequest;
 import com.codecooldev.functionalities.response.HttpResponse;
+import com.codecooldev.functionalities.response.HttpResponseCreatorService;
+import com.codecooldev.functionalities.response.HttpResponseService;
 
 
 import java.io.IOException;
